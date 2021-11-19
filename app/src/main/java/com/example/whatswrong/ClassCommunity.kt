f686a749dc2c0e2bebc30ebc735d5c2ee20e1a86
SangwonYoon <yoonsw0532@naver.com>
2021-11-19 16:36:51 +0900

    글쓰기 기능 추가

--- app/src/main/java/com/example/whatswrong/ClassCommunity.kt
+++ app/src/main/java/com/example/whatswrong/ClassCommunity.kt
@@ -1,5 +1,6 @@
 package com.example.whatswrong
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
@@ -38,6 +39,7 @@ class ClassCommunity  : AppCompatActivity(){
 
         myRef.addValueEventListener(object : ValueEventListener{
             override fun onDataChange(p0: DataSnapshot) {
+                postList.clear()
                 for(data in p0.children) {
                     val temp_post = data.getValue(Posts::class.java) // Posts class에 기본 생성자가 있어야 함
                     val post = Posts(temp_post?.title.toString(),temp_post?.content.toString(),temp_post?.user.toString(),temp_post?.time.toString())
@@ -56,18 +58,15 @@ class ClassCommunity  : AppCompatActivity(){
         postButton.setOnClickListener{
             Log.d("test", "버튼 눌림")
 
+            val intent = Intent(this@ClassCommunity, WritePost::class.java)
+            intent.putExtra("과목명", className)
+            startActivity(intent)
+
             // intent에서 넘어온 데이터에서 title, content, user, time을 추출
 
-            val post = Posts(
-            "안녕하세요!!",
-            "hi",
-            "익명",
-                "12:30"
-            )
+            //val post = Posts("안녕하세요!!", "hi", "익명","12:30")
 
-            myRef.push().setValue(post)
+            //myRef.push().setValue(post)
         }
-
-        // post 눌렀을 때 게시글로 이동
     }
 }
